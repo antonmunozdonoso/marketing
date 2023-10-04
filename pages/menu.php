a634d09c6590fb09776d3807d992c5399200b5dc
antonmunozdonoso <samerico1980@gmail.com>
2023-10-04 17:15:21 -0300

    modificando menu

--- pages/menu.php
+++ pages/menu.php
@@ -25,7 +25,7 @@
             <span class="nav-link-text ms-1">Billing</span>
           </a>
         </li>
-        <li class="nav-item">
+        <!--<li class="nav-item">
           <a class="nav-link text-white " href="../pages/virtual-reality.html">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">view_in_ar</i>
@@ -75,7 +75,7 @@
             </div>
             <span class="nav-link-text ms-1">Sign Up</span>
           </a>
-        </li>
+        </li>-->
       </ul>
     </div>
     <div class="sidenav-footer position-absolute w-100 bottom-0 ">
